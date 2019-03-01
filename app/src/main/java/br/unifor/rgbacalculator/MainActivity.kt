862e61ca210ce01cb04984665bb7986d047b7302
josevictor <jose.victor@edu.unifor.br>
2019-03-01 19:36:19 -0300

    layout pronto

--- app/src/main/java/br/unifor/rgbacalculator/MainActivity.kt
+++ app/src/main/java/br/unifor/rgbacalculator/MainActivity.kt
@@ -1,59 +1,74 @@
 package br.unifor.rgbacalculator
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
 import android.text.method.DigitsKeyListener
-import android.widget.EditText
-import android.widget.SeekBar
-import android.widget.Switch
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var mEditTextIntensityRed: EditText
-    private lateinit var mEditTextIntensityGreen: EditText
-    private lateinit var mEditTextIntensityBlue: EditText
-    private lateinit var mEditTextIntensityAlpha: EditText
-    private lateinit var mSeekBarIntensityRed: SeekBar
-    private lateinit var mSeekBarIntensityGreen: SeekBar
-    private lateinit var mSeekBarIntensityBlue: SeekBar
-    private lateinit var mSeekBarIntensityAlpha: SeekBar
-    private lateinit var mSwitchActivateHex: Switch
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        this.mEditTextIntensityRed = findViewById(R.id.main_edittext_intensity_red)
-        this.mEditTextIntensityRed.inputType = InputType.TYPE_CLASS_NUMBER
 
-        this.mEditTextIntensityGreen = findViewById(R.id.main_edittext_intensity_green)
-        this.mEditTextIntensityBlue = findViewById(R.id.main_edittext_intensity_blue)
-        this.mEditTextIntensityAlpha = findViewById(R.id.main_edittext_intensity_alpha)
-        this.mSeekBarIntensityRed = findViewById(R.id.main_seekBar_intensity_red)
-        this.mSeekBarIntensityGreen = findViewById(R.id.main_seekBar_intensity_green)
-        this.mSeekBarIntensityBlue = findViewById(R.id.main_seekBar_intensity_blue)
-        this.mSeekBarIntensityAlpha = findViewById(R.id.main_seekBar_intensity_alpha)
-        this.mSwitchActivateHex = findViewById(R.id.main_switch_activate_hex)
-
-        this.mSwitchActivateHex.setOnCheckedChangeListener{ buttonView, isChecked ->
+        main_switch_activate_hex.setOnCheckedChangeListener{ buttonView, isChecked ->
             if (isChecked) {
                 Toast.makeText(this, "Hexadecimal ativado !", Toast.LENGTH_LONG).show()
-                this.mEditTextIntensityRed.hint = "FFF"
-                this.mEditTextIntensityRed.keyListener = DigitsKeyListener.getInstance("ABCDEF")
-                this.mEditTextIntensityRed.inputType = InputType.TYPE_CLASS_TEXT
+                main_edittext_intensity_red.hint = "FFF"
+                main_edittext_intensity_red.keyListener = DigitsKeyListener.getInstance("ABCDEF")
+                main_edittext_intensity_red.inputType = InputType.TYPE_CLASS_TEXT
             }
             else {
                 Toast.makeText(this, "Hexadecimal desativado !", Toast.LENGTH_LONG).show()
-                this.mEditTextIntensityRed.hint = "255"
-                this.mEditTextIntensityRed.keyListener = DigitsKeyListener.getInstance("0123456789")
-                this.mEditTextIntensityRed.inputType = InputType.TYPE_CLASS_NUMBER
+                main_edittext_intensity_red.hint = "255"
+                main_edittext_intensity_red.keyListener = DigitsKeyListener.getInstance("0123456789")
+                main_edittext_intensity_red.inputType = InputType.TYPE_CLASS_NUMBER
             }
 
         }
-        
-        this.mSeekBarIntensityRed.setOnSeekBarChangeListener(SeekBarEvent())
+
+
+        main_seekBar_intensity_red.setOnSeekBarChangeListener(SeekBarEvent { progress ->
+            //val valueAlpha = main_seekBar_intensity_alpha.progress * 255 / 100
+            val valueRed = progress * 255 / 100
+            val valueGreen = main_seekBar_intensity_green.progress * 255 / 100
+            val valueBlue = main_seekBar_intensity_blue.progress * 255 / 100
+            val colorApplyImage = Color.rgb(valueRed, valueGreen, valueBlue)
+            main_imageview_logo_unifor.setColorFilter(colorApplyImage)
+        })
+
+
+        main_seekBar_intensity_green.setOnSeekBarChangeListener(SeekBarEvent { progress ->
+            //val valueAlpha = main_seekBar_intensity_alpha.progress * 255 / 100
+            val valueRed = main_seekBar_intensity_red.progress * 255 / 100
+            val valueGreen = progress * 255 / 100
+            val valueBlue = main_seekBar_intensity_blue.progress * 255 / 100
+            val colorApplyImage = Color.rgb(valueRed, valueGreen, valueBlue)
+            main_imageview_logo_unifor.setColorFilter(colorApplyImage)
+        })
+
+        main_seekBar_intensity_blue.setOnSeekBarChangeListener(SeekBarEvent {progress ->
+            //val valueAlpha = main_seekBar_intensity_alpha.progress * 255 / 100
+            val valueRed = main_seekBar_intensity_red.progress * 255 / 100
+            val valueGreen = main_seekBar_intensity_green.progress * 255 / 100
+            val valueBlue = progress * 255 / 100
+            val colorApplyImage = Color.rgb(valueRed, valueGreen, valueBlue)
+            main_imageview_logo_unifor.setColorFilter(colorApplyImage)
+        })
+
+        main_seekBar_intensity_alpha.setOnSeekBarChangeListener(SeekBarEvent {progress ->
+            val valueRed = main_seekBar_intensity_red.progress * 255 / 100
+            val valueGreen = main_seekBar_intensity_green.progress * 255 / 100
+            val valueBlue = main_seekBar_intensity_blue.progress * 255 / 100
+            val colorApplyImage = Color.rgb(valueRed, valueGreen, valueBlue)
+
+            main_imageview_logo_unifor.setColorFilter(colorApplyImage)
+            main_imageview_logo_unifor.imageAlpha = progress
+        })
 
         //this.window.decorView.setBackgroundColor(Color.parseColor("#2196F3"))
 
--- app/src/main/java/br/unifor/rgbacalculator/SeekBarEvent.kt
+++ app/src/main/java/br/unifor/rgbacalculator/SeekBarEvent.kt
@@ -5,12 +5,9 @@ import android.widget.SeekBar
 import android.graphics.Color
 
 
-class SeekBarEvent : SeekBar.OnSeekBarChangeListener {
+class SeekBarEvent(val onChange: (progress: Int) -> Unit) : SeekBar.OnSeekBarChangeListener {
     override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-        val lower = Color.argb(0xFF, progress, progress, progress)
-        //val sharp = Bitmap.createBitmap(back) // create a copy
-        //ManipBitmap.sharpen(sharp, lower)
-        //image.setImageBitmap(sharp)
+        onChange(progress)
     }
 
     override fun onStartTrackingTouch(seekBar: SeekBar?) {}
